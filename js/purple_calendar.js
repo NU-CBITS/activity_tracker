c22e6c72aee8071c8337a77b7563038025e91ef4
michaelwehrley <michael.wehrley@gmail.com>
2013-05-08 14:29:48 -0500

    mobile calendar is in place

--- js/purple_calendar.js
+++ js/purple_calendar.js
@@ -25,71 +25,82 @@ window.PurpleCalendar = (function() {
 
   Calendar.prototype.load = function() {
     var self = this;
-    $(this.container).prepend('<div id="calendar"></div>');
-    this.$el = $(this.container).find('div#calendar');
-    var height = $("#visualizations-outer-container").height() - 40;
-    
-    this.$el.fullCalendar({
-      height:height,
-      header: {
-        left: 'agendaDay,agendaWeek,month',
-        center: 'title',
-        right: 'prev,next today'
-      },
-      // 
-      selectable: true,
-      editable: true,
-      events: function(start, end, callback) {
+    var schedulerHTML = '<div id="scheduler_here" class="dhx_cal_container" style="width:100%; height:500px;""><div class="dhx_cal_navline"><div class="dhx_cal_prev_button">&nbsp;</div><div class="dhx_cal_next_button">&nbsp;</div><div class="dhx_cal_today_button"></div><div class="dhx_cal_date"></div><div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div><div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div><div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div></div><div class="dhx_cal_header"></div><div class="dhx_cal_data"></div></div>'
+    // if ((screen.width < 480) || (screen.height < 480)) {
+    if ((document.width < 480) || (document.height < 480)) {
 
-        var eventsInPeriod, eventObjs;
+      $(this.container).prepend(schedulerHTML);
+      scheduler.init('scheduler_here',null,"day");
 
-        var filtered_events = self.filteredEventsFn();
+    } else {
 
-        eventsInPeriod = filtered_events.filter(function(event) { 
-          return (
-            ( event.get_field_value('start') >= start && event.get_field_value('start') <= end ) || 
-            ( event.get_field_value('end') >= start && event.get_field_value('end') <= end )
-          );
-        });
+      $(this.container).prepend('<div id="calendar"></div>');
+      this.$el = $(this.container).find('div#calendar');
+      var height = $("#visualizations-outer-container").height() - 40;
 
-        eventObjs = eventsInPeriod.map(function(event) { 
-          return _.extend({id: event.cid}, event.get_fields_as_object()) 
-        });
+      this.$el.fullCalendar({
+        height:height,
+        header: {
+          left: 'agendaDay,agendaWeek,month',
+          center: 'title',
+          right: 'prev,next today'
+        },
+        // 
+        selectable: true,
+        editable: true,
+        events: function(start, end, callback) {
 
-        callback(eventObjs);
-      },
-      
-      eventClick: function(calEvent, jsEvent, view) {
-        var acEvent = ActivityCalEvents.get(calEvent.id);
-        editEventView.updateModel( acEvent );
-        //editEventView should have an updated start time and an updated 'currentState'
-        $('div#edit-event-container').openForm().effect("highlight", {}, 1000);
-      },
+          var eventsInPeriod, eventObjs;
 
-      dayClick: function(timeClicked, allDay, jsEvent, view) {
-        var y = timeClicked.getFullYear(),
-            m = timeClicked.getMonth(),
-            d = timeClicked.getDate(),
-            h = timeClicked.getHours(),
-            min = timeClicked.getMinutes();
+          var filtered_events = self.filteredEventsFn();
 
-        if (allDay) {
-          h = 12;
-          min = 0;
-        };
+          eventsInPeriod = filtered_events.filter(function(event) { 
+            return (
+              ( event.get_field_value('start') >= start && event.get_field_value('start') <= end ) || 
+              ( event.get_field_value('end') >= start && event.get_field_value('end') <= end )
+            );
+          });
 
-        var newModel = new ActivityCalEvent(),
-            s = new Date(y, m, d, h, min),
-            e = new Date(y, m, d, h+1, min);
-        newModel.set_field('start', 'datetime', s);
-        newModel.set_field('end', 'datetime', e);
-        //m should have the correct start time.
-        editEventView.updateModel(newModel);
-        //editEventView should have an updated start time
-        //and an updated 'currentState'
-        $('div#edit-event-container').openForm().effect("highlight", {}, 1000);
-      }    
-    });
+          eventObjs = eventsInPeriod.map(function(event) { 
+            return _.extend({id: event.cid}, event.get_fields_as_object()) 
+          });
+
+          callback(eventObjs);
+        },
+        
+        eventClick: function(calEvent, jsEvent, view) {
+          var acEvent = ActivityCalEvents.get(calEvent.id);
+          editEventView.updateModel( acEvent );
+          //editEventView should have an updated start time and an updated 'currentState'
+          $('div#edit-event-container').openForm().effect("highlight", {}, 1000);
+        },
+
+        dayClick: function(timeClicked, allDay, jsEvent, view) {
+          var y = timeClicked.getFullYear(),
+              m = timeClicked.getMonth(),
+              d = timeClicked.getDate(),
+              h = timeClicked.getHours(),
+              min = timeClicked.getMinutes();
+
+          if (allDay) {
+            h = 12;
+            min = 0;
+          };
+
+          var newModel = new ActivityCalEvent(),
+              s = new Date(y, m, d, h, min),
+              e = new Date(y, m, d, h+1, min);
+          newModel.set_field('start', 'datetime', s);
+          newModel.set_field('end', 'datetime', e);
+          //m should have the correct start time.
+          editEventView.updateModel(newModel);
+          //editEventView should have an updated start time
+          //and an updated 'currentState'
+          $('div#edit-event-container').openForm().effect("highlight", {}, 1000);
+        }    
+      });
+
+    }
 
   };
 
